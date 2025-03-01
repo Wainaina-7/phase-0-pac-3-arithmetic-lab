c3e1d5bd70b07ceb665863cb8f69183a370792db
Peter kanyori <peterynaenah23@gmail.com>
2025-03-01 11:35:03 +0300

    arithmetic

---
+++ index.js
@@ -0,0 +1,45 @@
+function add(a, b) {
+    return a + b
+  }
+
+  function subtract() {
+    return a - b
+  }
+  function multiply() {
+    return a * b
+  }
+  function divide() {
+return a / b
+  }
+  function add(a, b) {
+    return a + b
+  }
+  function subtract(a, b) {
+    return a - b
+  }
+  function add(a, b) {
+    return a + b
+  }
+  function multiply(a, b){
+    return a * b
+  }
+  function divide(a, b) {
+    return a / b
+  }
+  function increment(n) {
+    return n + 1
+}
+function decrement(n) {
+    return n - 1
+}
+function makeInt(n) {
+    const parsed = parseInt(n, 10);
+    return isNaN(parsed) ? NaN : parsed;
+  }
+ 
+  function preserveDecimal(n) {
+    return parseFloat(n);
+  }
+  
+  
+  
